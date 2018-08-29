07864608ae330595b787b7bb1482196b6c0cca81
yuxiqian <akaza_akari@sjtu.edu.cn>
2018-08-29 09:31:00 +0800

    Proto version.
    There's nothing inside!

---
+++ courseSync/courseSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  courseSync
+//
+//  Created by 法好 on 2018/8/29.
+//  Copyright © 2018 yuxiqian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
