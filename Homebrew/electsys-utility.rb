6b1ab1c93aac473814f92577473b83e20c68ce7b
yuetsin <akaza_akari@sjtu.edu.cn>
2020-12-12 17:40:58 +0800

    [update] homebrew recipe

--- Homebrew/electsys-utility.rb
+++ Homebrew/electsys-utility.rb
@@ -1,5 +1,5 @@
 cask 'electsys-utility' do
-    version '0.9.5'
+    version '0.9.9'
     sha256 :no_check
     url "https://github.com/yuetsin/electsys-utility/releases/download/v#{version}/Electsys.Utility.dmg"
     appcast 'https://github.com/yuetsin/electsys-utility/releases.atom'
